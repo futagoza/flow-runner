1ee098927ac5a99754db346062cee84d353a5dc7
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2016-11-12 20:03:20 +0000

    fixed '@inlineArguments' macro
    I forgot that `0` is the same as `false` to `while` statements, so using `--i` leaves out the first argument.

--- build/macros.js
+++ build/macros.js
@@ -10,8 +10,8 @@ module.exports = {
     prefix = prefix || this.ws || ''
     eol = eol || fs.EOL
 
-    return prefix + `let ${ var_ } = [], i = arguments.length` + eol +
-           prefix + `while ( --i ) ${ var_ }[ i ] = arguments[ i ]`
+    return prefix + `let ${ var_ } = [], i = -1, l = arguments.length` + eol +
+           prefix + `while ( ++i < l ) ${ var_ }[ i ] = arguments[ i ]`
 
   },
 
